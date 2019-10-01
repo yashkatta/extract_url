183889919410558704796d0808a7b52fc912bdc5
yashkatta <yash.katta1@gmail.com>
2019-10-01 14:36:14 +0530

    Added more filters

--- extract_url.sh
+++ extract_url.sh
@@ -5,13 +5,13 @@ schema="(https?|phar|ftp|smtp)"
 
 find_dir(){ 
 	cd $1
-	grep -rhEo "$schema:[^\"< \']+"
+	grep -rhEo "$schema:[^\"< \']+" | sed -e's/\/\+/ /g' | sed 's/://g'
 	cd - 1>/dev/null
 }
 
 find_web_page()
 {
-	curl $1 2>/dev/null | grep -Eo "$schema:[^\"< \']+"
+	curl $1 2>/dev/null | grep -Eo "$schema:[^\"< \']+" | sed -e's/\/\+/ /g' | sed 's/://g'
 }
 
 usage()
